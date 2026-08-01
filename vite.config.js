d07322b353bbaeb45348fb69958a643e84efda78
Prince Timilsina <timilsinaprince07@gmail.com>
2026-08-01 18:07:23 +0545

    Revert to automatic JSX runtime

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react({ jsxRuntime: "classic" })],
-  build: {
-    minify: false,
-  },
+  plugins: [react()],
 });
