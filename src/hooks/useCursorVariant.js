4b85f535227ba1fbe8476ce15450baab0695fa11
Prince Timilsina <timilsinaprince07@gmail.com>
2026-08-01 18:39:26 +0545

    deploy

--- src/hooks/useCursorVariant.js
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const CursorContext = createContext({ setVariant: () => {} });
-
-export const useCursorVariant = () => useContext(CursorContext);
